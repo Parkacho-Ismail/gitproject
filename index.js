10b53d5120d9ea97919950c76d245f3504027682
Parkacho ISmail <parkachoismail@gmail.com>
2023-07-25 11:24:26 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//console.log("Hello");
+//console.log("You look good!");
+//window.alert("I really like you");
+//this is a comment
+/*
+This is a multi line comment
+*/ 
+/*let firstName = "Pk";
+let age = 21;
+let student = true;
+
+age = age+1;
+
+console.log("Hello ",firstName);
+console.log("You are ",age, " years old");
+console.log("Enrolled ",student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "Your are " +age+ " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " +student;*/
+
+/*let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myLabel").innerHTML = username;
+}*/
+/*let fruits = ["Banana","Apple","Orange","Mango"];
+//fruits = fruits.sort();
+fruits.push("Guava");
+fruits.unshift("Straw Berry");
+//fruits = fruits.sort().reverse();
+for(let fruit of fruits){
+    console.log(fruit);
+}*/
+/*let class1 = ["Becky","John","David"];
+let class2 = ["Ana","Sam","Mary"];
+class1.push(...class2);//adds second array to first array
+console.log(class1);*/
+//let total = sum(3,2);
+console.log(sum(3,2));
+
+function sum(x,y){
+   // let result = x+y;
+    return x+y;
+}
+
